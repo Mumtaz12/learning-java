aec4eaab392702bc74fc873a031ccb20289a3300
bram <nogrend@msn.com>
2022-04-17 16:51:23 +0200

    add Sum Calculator exercise

---
+++ Section06/SumCalculator/SumCalculator.java
@@ -0,0 +1,41 @@
+package Section06.SumCalculator;
+
+public class SumCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return this.firstNumber + this.secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return this.firstNumber - this.secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return this.firstNumber * this.secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        }
+        return this.firstNumber / this.secondNumber;
+    }
+}
