c90e1cddfed801cb8246ca3fc756d747c8ccd248
bram <nogrend@msn.com>
2022-04-17 15:33:46 +0200

    first commit from intellij

--- Section05/PaintJob/PaintJobTest.java
+++ Section05/PaintJob/PaintJobTest.java
@@ -1,8 +1,8 @@
 package Section05.PaintJob;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PaintJobTest {
     @Test
---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello there");
+        System.out.println("hello there");
+    }
+}
