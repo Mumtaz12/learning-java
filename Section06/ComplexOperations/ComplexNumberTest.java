7b068e40049f1768884b2e6056869a658e48abd4
BOUB <bram.bouwens@witteveenbos.com>
2022-04-29 12:56:24 +0200

    add exercise complex number

--- Section06/ComplexOperations/ComplexNumberTest.java
+++ Section06/ComplexOperations/ComplexNumberTest.java
@@ -6,8 +6,28 @@ import static org.junit.Assert.assertEquals;
 
 public class ComplexNumberTest {
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testAddRealAndImaginaryNumber() {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        one.add(1, 1);
+        assertEquals(2.0, one.getReal(), 0.1);
+        assertEquals(2.0, one.getImaginary(), 0.1);
+    }
 
+    @Test
+    public void testSubtractRealAndImaginaryFromNumber() {
+        ComplexNumber one = new ComplexNumber(2.0, 2.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.subtract(number);
+        assertEquals(-0.5, one.getReal(), 0.1);
+        assertEquals(3.5, one.getImaginary(), 0.1);
+    }
+
+    @Test
+    public void testSubtractRealAndImaginaryFromOne() {
+        ComplexNumber one = new ComplexNumber(-0.5, 3.5);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        number.subtract(one);
+        assertEquals(3.0, number.getReal(), 0.1);
+        assertEquals(-5.0, number.getImaginary(), 0.1);
     }
 }
