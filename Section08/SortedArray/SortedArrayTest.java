323019396c4988a4fa2a4bc5987a141c2e1339cd
bram <nogrend@msn.com>
2022-05-09 22:18:45 +0200

    add exercise array test

--- Section08/SortedArray/SortedArrayTest.java
+++ Section08/SortedArray/SortedArrayTest.java
@@ -2,11 +2,17 @@ package Section08.SortedArray;
 
 import org.junit.Test;
 
+import static Section08.SortedArray.SortedArray.sortIntegers;
 import static org.junit.Assert.assertEquals;
 
 public class SortedArrayTest {
     @Test
     public void test() {
-        assertEquals(true, true);
+        int[] unsortedArray = {23, 40, 21, 5, 33};
+        int[] descendingArray = {40, 33, 23, 21, 5};
+        int[] sortedArray = sortIntegers(unsortedArray);
+        for (int i = 0; i < unsortedArray.length; i++) {
+            assertEquals(descendingArray[i], sortedArray[i]);
+        }
     }
 }
