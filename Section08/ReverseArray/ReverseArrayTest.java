1ab52e715f9666542619348b1ce9b08611b8af14
BOUB <bram.bouwens@witteveenbos.com>
2022-05-11 12:24:31 +0200

    change reversedArray initiation to anonymous array

--- Section08/ReverseArray/ReverseArrayTest.java
+++ Section08/ReverseArray/ReverseArrayTest.java
@@ -9,7 +9,7 @@ public class ReverseArrayTest {
     public void reverseArray() {
         int[] array = { 1, 2, 3, 4, 5 };
         ReverseArray.reverse(array);
-        int[] reversedArray = new int[] { 5, 4, 3, 2, 1 };
+        int[] reversedArray = { 5, 4, 3, 2, 1 };
         for (int i = 0; i < array.length; i++) {
             assertEquals(reversedArray[i], array[i]);
         }
