99dec0ca328cf262db7ed5823f02d974d845d57b
BOUB <bram.bouwens@witteveenbos.com>
2022-05-11 12:17:59 +0200

    add exercise 43 reverse array

---
+++ Section08/ReverseArray/ReverseArrayTest.java
@@ -0,0 +1,18 @@
+package Section08.ReverseArray;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ReverseArrayTest {
+    @Test
+    public void reverseArray() {
+        int[] array = { 1, 2, 3, 4, 5 };
+        ReverseArray.reverse(array);
+        int[] reversedArray = new int[] { 5, 4, 3, 2, 1 };
+        for (int i = 0; i < array.length; i++) {
+            assertEquals(reversedArray[i], array[i]);
+        }
+
+    }
+}
