95fd56442674405dc8671aeed3e5f63578755904
BOUB <bram.bouwens@witteveenbos.com>
2022-05-24 08:30:27 +0200

    adjust tests for initial transaction in constructor

--- Section08/Banking/CustomerTest.java
+++ Section08/Banking/CustomerTest.java
@@ -11,54 +11,46 @@ import static org.junit.Assert.assertNotEquals;
 public class CustomerTest {
     @Test
     public void getName() {
-        Customer customer = new Customer("Billy");
+        Customer customer = new Customer("Billy", 10.00);
         assertEquals("Billy", customer.getName());
     }
 
     @Test
     public void getNameFails() {
-        Customer customer = new Customer("Billy");
+        Customer customer = new Customer("Billy", 10.00);
         assertNotEquals("Carl", customer.getName());
+        
+        double firstTransactionInBank = customer.getTransactions().get(0);
+        assertEquals(10.00, firstTransactionInBank, 0.001);
     }
 
     @Test
-    public void addAndgetBillysTransaction() {
-        Customer customer = new Customer("Billy");
+    public void addAndgetBillysFirstTwoTransactions() {
+        double firstTransaction = 10.00;
+        Customer customer = new Customer("Billy", firstTransaction);
 
-        double transaction = 10.33;
-        customer.addTransaction(transaction);
-        double firstTransaction = customer.getTransactions().get(0);
+        double secondTransaction = 10.33;
+        customer.addTransaction(secondTransaction);
 
-        assertEquals(10.33, firstTransaction, 0.001);
+        double firstTransactionInBank = customer.getTransactions().get(0);
+        assertEquals(10.00, firstTransactionInBank, 0.001);
+
+        double secondTransactionInBank = customer.getTransactions().get(1);
+        assertEquals(secondTransaction, secondTransactionInBank, 0.001);
     }
 
-    @Test
-    public void addAndgetBillysTwoTransactions() {
-        Customer customer = new Customer("Billy");
-
-        double transaction1 = 10.33;
-        customer.addTransaction(transaction1);
-        double transaction2 = 1000.00;
-        customer.addTransaction(transaction2);
-
-        ArrayList<Double> billysTransactions = customer.getTransactions();
-
-        double firstTransaction = customer.getTransactions().get(0);
-        assertEquals(10.33, firstTransaction, 0.001);
-
-        double secondTransaction = customer.getTransactions().get(1);
-        assertEquals(1000.00, secondTransaction, 0.001);
-
-    }
 
     @Test
     public void addAndgetBillysFiveTransactions() {
-        Customer customer = new Customer("Billy");
+        double firstTransaction = 10.00;
+        Customer customer = new Customer("Billy", firstTransaction);
 
         ArrayList<Double> transactions = new ArrayList<>(Arrays.asList(10.30, 1000.00, 30.30, 1111.30, 1030.22, 30.29));
         for (int i = 0; i < transactions.size(); i++) {
             customer.addTransaction(transactions.get(i));
         }
+
+        transactions.add(0, firstTransaction);
 
         ArrayList<Double> billysTransactions = customer.getTransactions();
         for (int i = 0; i < billysTransactions.size(); i++) {
