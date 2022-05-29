0b1df0285a962eb79667232ad2fefa8637bac36a
bram <nogrend@msn.com>
2022-05-29 22:20:14 +0200

    WIP add tests for Bank

--- Section08/Banking/BankTest.java
+++ Section08/Banking/BankTest.java
@@ -1,4 +1,22 @@
 package Section08.Banking;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BankTest {
+    @Test
+    public void addBranch() {
+        Bank bank = new Bank("Big Bank");
+        boolean isBranchAdded = bank.addBranch("Amsterdam");
+        assertEquals(true, isBranchAdded);
+    }
+
+    @Test
+    public void addBranchWithoutUniqueName() {
+        Bank bank = new Bank("Big Bank");
+        bank.addBranch("Amsterdam");
+        boolean isBranchAdded = bank.addBranch("Amsterdam");
+        assertEquals(false, isBranchAdded);
+    }
 }
