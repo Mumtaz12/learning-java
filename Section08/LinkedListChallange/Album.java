8511b448fe9f1b1e0bfbeb48f03a946d5b5f2805
bram <nogrend@msn.com>
2022-06-18 23:05:46 +0200

    WIP add linked list challange

---
+++ Section08/LinkedListChallange/Album.java
@@ -0,0 +1,52 @@
+package Section08.LinkedListChallange;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song checkedSong : songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if ((index >= 0) && (index <= songs.size())) {
+            playList.add(songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song song = findSong(title);
+        if (song != null) {
+            playList.add(song);
+        }
+        System.out.println("This album does not hava a track with title " + title);
+        return false;
+    }
+}
---
+++ Section08/LinkedListChallange/Main.java
@@ -0,0 +1,7 @@
+package Section08.LinkedListChallange;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
